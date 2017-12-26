1a41f26436b127776daa3d88c2a8e42b66e3f8a9
Tejas Pant <tejaspant55@gmail.com>
2017-12-26 15:50:55 +0545

    Question 3 submitted

---
+++ question3.py
@@ -0,0 +1,4 @@
+#a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized
+
+word = str(input("Enter the string: "))
+print("Given string in capitalized form is '{:s}'.".format(word.upper()))
